d7f41fdfcc565c795e0104d02798bff625186ea0
Maria Jeffina <47268934+mariaajeffina@users.noreply.github.com>
2019-09-11 23:45:47 +0700

    initial commit

---
+++ WebApplication3/WebApplication3/Models/ProductContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace WebApplication3.Models
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext() : base("WebApplication3")
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        
+    }
+
+
+}
