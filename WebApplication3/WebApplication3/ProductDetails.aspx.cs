8946b58dc084db5ab4ce5bd3ed403e45093d5a11
Maria Jeffina <47268934+mariaajeffina@users.noreply.github.com>
2019-09-12 12:36:34 +0700

    added item description

--- WebApplication3/WebApplication3/ProductDetails.aspx.cs
+++ WebApplication3/WebApplication3/ProductDetails.aspx.cs
@@ -5,24 +5,27 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using WebApplication3.Models;
-using System.Web.ModelBinding; //untuk binding ke cantolan yang udh dibikin
+using System.Web.ModelBinding;
 
 namespace WebApplication3
 {
-    public partial class ProductList : System.Web.UI.Page
+    public partial class ProductDetails : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
-        //so now we're going to populate it
-        public IQueryable<Product> GetProducts([QueryString("id")] int? categoryId)
+        public IQueryable<Product> GetProduct([ QueryString("productID")] int? productId)
         {
             var _db = new WebApplication3.Models.ProductContext();
             IQueryable<Product> query = _db.Products;
-            if (categoryId.HasValue && categoryId > 0)
+            if (productId.HasValue && productId > 0)
             {
-                query = query.Where(p => p.CategoryID == categoryId);
+                query = query.Where(p => p.ProductID == productId);
+            }
+            else
+            {
+                query = null;
             }
             return query;
         }
